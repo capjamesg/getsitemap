21c862f287f9b690e40ee82b555c927ca5a67b80
capjamesg <jamesg@jamesg.blog>
2022-10-09 19:32:50 +0100

    add dedupe_results param

--- src/getsitemap/retrieve_sitemap.py
+++ src/getsitemap/retrieve_sitemap.py
@@ -6,7 +6,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def _concurrent_thread_starter(urls: list, thread_max: int, allow_xml_inference: bool):
+def _concurrent_thread_starter(urls: list, thread_max: int, allow_xml_inference: bool, dedupe_results: bool):
     """
     Create a pool of threads to retrieve sitemap files.
 
@@ -22,7 +22,7 @@ def _concurrent_thread_starter(urls: list, thread_max: int, allow_xml_inference:
     results = {}
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=thread_max) as executor:
-        processes = [executor.submit(_get_individual_sitemap, url, allow_xml_inference) for url in urls]
+        processes = [executor.submit(_get_individual_sitemap, url, dedupe_results, allow_xml_inference) for url in urls]
 
         for process in concurrent.futures.as_completed(processes):
             results.update(process.result())
@@ -59,7 +59,7 @@ def _parse_list_of_urls(parsed_file: BeautifulSoup, root_url: str, allow_xml_inf
     return all_urls
 
 
-def _get_individual_sitemap(root_url: str, thread_max: int, allow_xml_inference: bool = True) -> dict:
+def _get_individual_sitemap(root_url: str, thread_max: int, dedupe_results: bool, allow_xml_inference: bool = True) -> dict:
     """
     Get all of the URLs associated with a single sitemap.
 
@@ -91,13 +91,16 @@ def _get_individual_sitemap(root_url: str, thread_max: int, allow_xml_inference:
 
         sitemap_urls = list(set([sitemap.find("loc").text for sitemap in all_sitemaps if sitemap.find("loc")]))
 
-        all_urls.update(_concurrent_thread_starter(sitemap_urls, thread_max, allow_xml_inference))
+        all_urls.update(_concurrent_thread_starter(sitemap_urls, thread_max, allow_xml_inference, dedupe_results))
     else:
         all_urls.update(_parse_list_of_urls(parsed_file, root_url, allow_xml_inference))
 
     for key, value in all_urls.items():
         # remove duplicates
-        all_urls[key] = list(set(value))
+        if dedupe_results:
+            all_urls[key] = list(set(value))
+        else:
+            all_urls[key] = value
 
     return all_urls
 
@@ -125,7 +128,7 @@ def _flatten_sitemap_dictionaries(all_discovered_sitemaps: dict) -> dict:
     return flat_sitemaps
 
 
-def retrieve_sitemap_urls(root_page: str, as_flat_list: bool = True, allow_xml_inference: bool = True, thread_max: int = 20) -> Union[list, dict]:
+def retrieve_sitemap_urls(root_page: str, as_flat_list: bool = True, allow_xml_inference: bool = True, thread_max: int = 20, dedupe_results: bool = True) -> Union[list, dict]:
     """
     Find all of the URLs in every sitemap associated with a provided domain.
 
@@ -166,7 +169,7 @@ def retrieve_sitemap_urls(root_page: str, as_flat_list: bool = True, allow_xml_i
 
         unique_sitemaps = list(set(sitemap_urls))
 
-        new_urls = _concurrent_thread_starter(unique_sitemaps, thread_max, allow_xml_inference)
+        new_urls = _concurrent_thread_starter(unique_sitemaps, thread_max, allow_xml_inference, dedupe_results)
 
         all_discovered_urls.update(new_urls)
 
