4f1107c699c1ff0bf36f44a6b7239d16b80572b3
capjamesg <jamesg@jamesg.blog>
2022-10-09 19:29:26 +0100

    add more description to docstrings, create allow_xml_inference and thread_max variables

--- src/getsitemap/retrieve_sitemap.py
+++ src/getsitemap/retrieve_sitemap.py
@@ -6,11 +6,23 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def _concurrent_thread_starter(urls: list):
+def _concurrent_thread_starter(urls: list, thread_max: int, allow_xml_inference: bool):
+    """
+    Create a pool of threads to retrieve sitemap files.
+
+    :param urls: A list of URLs to retrieve.
+    :type urls: list
+    :param thread_max: The maximum number of threads to use in sitemap retrieval requests.
+    :type thread_max: int
+    :param allow_xml_inference: Whether or not to infer that a URL ending in .xml is a sitemap.
+    :type allow_xml_inference: bool
+    :return: A dictionary of URLs found in each discovered sitemap.
+    :rtype: dict
+    """
     results = {}
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
-        processes = [executor.submit(_get_individual_sitemap, url) for url in urls]
+    with concurrent.futures.ThreadPoolExecutor(max_workers=thread_max) as executor:
+        processes = [executor.submit(_get_individual_sitemap, url, allow_xml_inference) for url in urls]
 
         for process in concurrent.futures.as_completed(processes):
             results.update(process.result())
@@ -18,17 +30,26 @@ def _concurrent_thread_starter(urls: list):
     return results
 
 
-def _parse_list_of_urls(parsed_file: BeautifulSoup, root_url: str) -> dict:
+def _parse_list_of_urls(parsed_file: BeautifulSoup, root_url: str, allow_xml_inference: bool = True) -> dict:
     """
     Get all the URLs in a non-sitemapindex sitemap.
+
+    :param parsed_file: The parsed sitemap file.
+    :type parsed_file: BeautifulSoup
+    :param root_url: The URL of the sitemap.
+    :type root_url: str
+    :param allow_xml_inference: Whether or not to infer that a URL ending in .xml is a sitemap.
+    :type allow_xml_inference: bool
+    :return: A dictionary of URLs found in each discovered sitemap.
+    :rtype: dict
     """
     all_urls = {}
 
     for url in parsed_file.find_all("url"):
         if not url.find("loc") or not url.find("loc").text:
             continue
-        if url.find("loc") and url.find("loc").text.endswith(".xml"):
-            all_urls.update(_get_individual_sitemap(url.find("loc").text.strip()))
+        if url.find("loc") and url.find("loc").text.endswith(".xml") and allow_xml_inference:
+            all_urls.update(_get_individual_sitemap(url.find("loc").text.strip(), allow_xml_inference))
         elif url.find("loc"):
             if all_urls.get(root_url):
                 all_urls[root_url].append(url.find("loc").text.strip())
@@ -38,12 +59,16 @@ def _parse_list_of_urls(parsed_file: BeautifulSoup, root_url: str) -> dict:
     return all_urls
 
 
-def _get_individual_sitemap(root_url: str) -> dict:
+def _get_individual_sitemap(root_url: str, thread_max: int, allow_xml_inference: bool = True) -> dict:
     """
     Get all of the URLs associated with a single sitemap.
 
     :param root_url: The URL of the sitemap.
     :type root_url: str
+    :param thread_max: The maximum number of threads to use in sitemap retrieval requests.
+    :type thread_max: int
+    :param allow_xml_inference: Whether or not to infer that a URL ending in .xml is a sitemap.
+    :type allow_xml_inference: bool
     :return: A dictionary of URLs found in each discovered sitemap.
     :rtype: dict
     """
@@ -66,9 +91,9 @@ def _get_individual_sitemap(root_url: str) -> dict:
 
         sitemap_urls = list(set([sitemap.find("loc").text for sitemap in all_sitemaps if sitemap.find("loc")]))
 
-        all_urls.update(_concurrent_thread_starter(sitemap_urls))
+        all_urls.update(_concurrent_thread_starter(sitemap_urls, thread_max, allow_xml_inference))
     else:
-        all_urls.update(_parse_list_of_urls(parsed_file, root_url))
+        all_urls.update(_parse_list_of_urls(parsed_file, root_url, allow_xml_inference))
 
     for key, value in all_urls.items():
         # remove duplicates
@@ -100,7 +125,7 @@ def _flatten_sitemap_dictionaries(all_discovered_sitemaps: dict) -> dict:
     return flat_sitemaps
 
 
-def retrieve_sitemap_urls(root_page: str, as_flat_list: bool = True) -> Union[list, dict]:
+def retrieve_sitemap_urls(root_page: str, as_flat_list: bool = True, allow_xml_inference: bool = True, thread_max: int = 20) -> Union[list, dict]:
     """
     Find all of the URLs in every sitemap associated with a provided domain.
 
@@ -110,6 +135,10 @@ def retrieve_sitemap_urls(root_page: str, as_flat_list: bool = True) -> Union[li
     :type root_page: str
     :param as_flat_list: Whether or not to return the URLs as a flat list.
     :type as_flat_list: bool
+    :param allow_xml_inference: Whether or not to infer that a URL ending in .xml is a sitemap.
+    :type allow_xml_inference: bool
+    :param thread_max: The maximum number of threads to use in sitemap retrieval requests.
+    :type thread_max: int
     :return: A list of URLs.
     :rtype: Union[list, dict]
 
@@ -137,7 +166,7 @@ def retrieve_sitemap_urls(root_page: str, as_flat_list: bool = True) -> Union[li
 
         unique_sitemaps = list(set(sitemap_urls))
 
-        new_urls = _concurrent_thread_starter(unique_sitemaps)
+        new_urls = _concurrent_thread_starter(unique_sitemaps, thread_max, allow_xml_inference)
 
         all_discovered_urls.update(new_urls)
 
