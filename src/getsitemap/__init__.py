365504428e8cd9504b6524193639fda46e377011
capjamesg <jamesg@jamesg.blog>
2023-08-03 20:07:00 +0100

    remove old code

--- src/getsitemap/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-getsitemap Python library.
-"""
-
-from .retrieve_sitemap import get_individual_sitemap, retrieve_sitemap_urls
-
-__version__ = "0.1.2"
-
-__all__ = ["retrieve_sitemap_urls", "get_individual_sitemap"]
